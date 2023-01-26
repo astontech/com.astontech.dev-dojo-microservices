7f4dd8efe0660a0761c8f4cdced56c0d328c8310
Kory Sericati <kory.sericati@astontech.com>
2023-01-26 12:55:24 -0600

    Initial commit: Spring Cloud Config server and client demo apps with walkthrough docs

---
+++ config-client/src/main/java/com/astontech/devdojomicroservice/configclient/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.astontech.devdojomicroservice.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${user.role}")
+    private String role;
+
+    @GetMapping(value = "/role", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String getConfiguredRole() {
+        return role;
+    }
+}
